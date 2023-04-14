25be69f67cc80f9e6693184c9a18e13c6def15d2
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-14 12:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function createTable() {
     //Write your code here
+	  // Get number of rows from user input
+  var numRows = prompt("Input number of rows:");
+  
+  // Get number of columns from user input
+  var numCols = prompt("Input number of columns:");
+  
+  // Get table element
+  var table = document.getElementById("myTable");
+  
+  // Create rows and cells in the table
+  for (var i = 0; i < numRows; i++) {
+    var newRow = table.insertRow(i);
+    
+    for (var j = 0; j < numCols; j++) {
+      var newCell = newRow.insertCell(j);
+      newCell.innerHTML = "Row-" + (i) + " Column-" + (j);
+    }
+  }
   
 }
