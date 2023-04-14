7992a827475ac2f553b52f01b0cff793deb8bb92
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-14 12:17:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function createTable() {
     //Write your code here
 	  // Get number of rows from user input
-  var numRows = prompt("input number of rows:");
+  var numRows = prompt("input number of rows");
   
   // Get number of columns from user input
-  var numCols = prompt("input number of columns:");
+  var numCols = prompt("input number of columns");
   
   // Get table element
   var table = document.getElementById("myTable");
